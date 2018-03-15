38ffeb37029ab4c5128aa5f9f2d2046b1e6c1684
Justin <justin@homeceuconnection.com>
2018-03-15 10:13:51 -0500

    Added common module to library

--- src/app/NgLocationModule.ts
+++ src/app/NgLocationModule.ts
@@ -1,7 +1,11 @@
-import {NgModule} from '@angular/core';
+import {CommonModule}  from "@angular/common";
+import {NgModule}      from '@angular/core';
 import {LocationProxy} from "./Service/LocationProxy";
 
 @NgModule({
+    imports  : [
+        CommonModule
+    ],
     providers: [
         LocationProxy
     ]
